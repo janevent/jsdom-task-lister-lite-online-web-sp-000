4fe1fe6bdf5981a9e4239d0c052deed5fda52db9
Jane Venter <janeanneventer@gmail.com>
2019-10-16 04:46:45 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector('form').addEventListener("submit", function(event){
+  document.getElementById('create-task-form').addEventListener("submit", function(event){
     let input = document.getElementById("new-task-description").value;
     let tasks = document.getElementById("tasks");
     //let li = document.createElement("li");

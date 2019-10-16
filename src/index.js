731a89a81e01789b73771a3a1df9de419605bf72
Jane Venter <janeanneventer@gmail.com>
2019-10-16 04:14:49 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector('[type=submit]').addEventListener("click", function(event){
     event.preventDefault();
-  });
-    document.querySelectorAll('[type=submit]').addEventListenter("click", function(event){
+  }, false);
+  document.querySelector('[type=submit]').addEventListener("click",
+    function(event){
       let input = document.getElementById("new-task-description").value;
 
       let tasks = document.getElementById("tasks");
+      let li = document.createElement("li");
       let li = tasks.appendChild("li");
-      li.innerText = input;
-  });
+      li.innerHTML = input;
+  })
 
-});
+})

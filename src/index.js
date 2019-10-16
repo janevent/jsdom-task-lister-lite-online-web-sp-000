acf6e912323ff982c7bec8e7438bd453856ba965
Jane Venter <janeanneventer@gmail.com>
2019-10-16 05:15:24 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("hit")
     let input = document.getElementById("new-task-description").value;
     let tasks = document.getElementById("tasks");
-    //let li = document.createElement("li");
-    tasks.append(input);
+    let li = document.createElement("li");
+    li.innerHTML = input;
+    tasks.append(li);
     //li.innerHTML = input;
     event.preventDefault(); return false;
   });

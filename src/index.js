0b8961435026a6d0ed496f85916ba7e7734794ea
Jane Venter <janeanneventer@gmail.com>
2019-10-16 04:19:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelector('[type=submit]').addEventListener("click", function(event){
+    let input = document.getElementById("new-task-description").value;
+
+    let tasks = document.getElementById("tasks");
+    let li = document.createElement("li");
+    let li = tasks.appendChild("li");
+    li.innerHTML = input;
     event.preventDefault();
   }, false);
-  document.querySelector('[type=submit]').addEventListener("click",
-    function(event){
-      let input = document.getElementById("new-task-description").value;
 
-      let tasks = document.getElementById("tasks");
-      let li = document.createElement("li");
-      let li = tasks.appendChild("li");
-      li.innerHTML = input;
-  })
 
 })
